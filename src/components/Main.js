840d55720ab278ba931636752a4547abc1b44777
Vinicius Matheus <viniciusomagnifico@hotmail.com>
2025-02-10 15:36:44 -0300

    Colocamos o localStorage e arrumamos os componentes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import Form from './Form';
+import Tarefas from './Tarefas'
+
 import './main.css';
 
-// Form
-import { FaPlus } from 'react-icons/fa';
 
-// Tarefas
-import { FaEdit, FaWindowClose } from 'react-icons/fa';
 
 export default class Main extends Component {
   state = {
@@ -13,6 +12,25 @@ export default class Main extends Component {
     tarefas: [],
     index: -1,
   };
+
+
+  //salvar no localStorage
+
+  componentDidMount() { //não deixa perder os dados quando atualiza
+    const tarefas = JSON.parse(localStorage.getItem('tarefas'));
+    if(!tarefas) return;
+    this.setState({ tarefas });
+  }
+
+  componentDidUpdate(prevPropos, prevState) {
+    const { tarefas } = this.state; // função praa adicionar o localstorage
+
+    if ( tarefas === prevState.tarefas) return;
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    console.log('As tarefas mudaram', tarefas);
+  }
+
 
   handleSubmit = (e) => {
     e.preventDefault(); // Evita o comportamento padrão de atualizar a página
@@ -23,7 +41,7 @@ export default class Main extends Component {
     if (novaTarefa === '') return; // Previne adicionar tarefas vazias
     if (tarefas.indexOf(novaTarefa) !== -1) return; // Previne adicionar tarefas duplicadas
 
-const novasTarefas = [...tarefas]
+    const novasTarefas = [...tarefas]
 
     if(index === -1) {
       this.setState({
@@ -78,28 +96,17 @@ handleDelete = (e, index) => {
       <div className="main">
         <h1>Lista de tarefas</h1>
 
-        <form onSubmit={this.handleSubmit} action="#" className="form">
-          <input
-            onChange={this.handleChange}
-            type="text"
-            value={novaTarefa}
-          />
-          <button type="submit">
-            <FaPlus />
-          </button>
-        </form>
+      <Form handleSubmit={this.handleSubmit}
+      handleChange={this.handleChange}
+      novaTarefa={novaTarefa}
+      />
 
-        <ul className="tarefas">
-          {tarefas.map((tarefa, index) => (
-            <li key={index}> {/* Usando "index" como key */}
-              {tarefa}
-              <span>
-                <FaEdit onClick={(e) => this.handleEdit(e, index)} className="edit" />
-                <FaWindowClose onClick={(e) => this.handleDelete(e, index)} className="delete" />
-              </span>
-            </li>
-          ))}
-        </ul>
+      <Tarefas
+      tarefas={tarefas}
+      handleEdit={this.handleEdit}
+      handleDelete={this.handleDelete} />
+
+
       </div>
     );
   }
